ea34f81e30bb06222532dfd8ae59b9cbf7d42050
Jeff Louisma <jefflouisma@me.com>
2026-01-19 16:15:09 -0800

    debug(fenrir): add EGL/Mesa debug logging for GBM init failure

--- fenrir/pkg/controllers/session.go
+++ fenrir/pkg/controllers/session.go
@@ -1133,6 +1133,10 @@ echo "=== Done ==="
 				// Tell libgbm where to find nvidia-drm_gbm.so (created by startup.sh)
 				// Default libgbm search path is /usr/lib/gbm/ (NOT /usr/lib/x86_64-linux-gnu/gbm/)
 				"GBM_BACKENDS_PATH": "/usr/lib/gbm",
+				// Debug: verbose EGL/Mesa logging to diagnose GBM init failure
+				"EGL_LOG_LEVEL":  "debug",
+				"MESA_DEBUG":     "1",
+				"LIBGL_DEBUG":    "verbose",
 				"LD_LIBRARY_PATH":   "/nvidia-libs:/nvidia-userspace:/usr/local/nvidia/lib:/usr/local/nvidia/lib64:/usr/local/lib",
 				// DRI device group access - Harvester host GIDs
 				// Wolf's 15-setup_devices.sh uses GOW_ prefix
